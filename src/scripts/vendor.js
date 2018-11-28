8adce5901c16ee0353760e65a4d42e5a3a95abde
piccolazucca <milena.zuccarelli@gmail.com>
2018-11-28 14:00:14 +0000

    Added image optimiser script and sorted others

--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -10,8 +10,9 @@
 // =require vendor/ajaxinate.min.js
 // =require vendor/lazysizes.min.js
 // =require vendor/ls.rias.min.js
+// =require vendor/ls.optimumx.min.js
 // =require vendor/siema.js
-// =require newsletter.js
+// =require modules/newsletter.js
 
 // Attempts to preserve comments that likely contain licensing information,
 // even if the comment does not have directives such as `@license` or `/*!`.
